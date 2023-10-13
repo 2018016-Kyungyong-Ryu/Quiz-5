687d4331f59b3ff36a432c1d6bbbb7494ce4438d
2018016-Kyungyong-Ryu <144421263+2018016-Kyungyong-Ryu@users.noreply.github.com>
2023-10-13 15:50:18 +0900

    7주차 과제 제출입니다. 붕어빵의 객체는 크림, 팥, 김치, 피자 총 4개이고, input 함수를 사용해 어떤 붕어빵을 총 몇개를 살지를 물어본다음 총 얼마가 나오는지를 출력하는 프로그램을 만들어봤습니다.

--- Quiz_5.py
+++ Quiz_5.py
@@ -20,8 +20,6 @@ kimchi = bread("김치", 1500)
 # 피자 붕어빵 객체 생성
 pizza = bread("피자", 1500)
 
-
-
 while True:
     choice = input("붕어빵을 고르세요 : ")
     b = int(input("몇개 고르실건가요? : "))
@@ -30,20 +28,24 @@ while True:
             b = b-1
             cream.sell()
         print("총 " + str(cream.total) + "이 나왔습니다.")
+        cream.eat()
     elif choice == "팥":
         while b>=1:
             b = b-1
             bean.sell()
         print("총 " + str(bean.total) + "가 나왔습니다.")
+        bean.eat()
     elif choice == "김치":
         while b>=1:
             b = b-1
             kimchi.sell()
         print("총 " + str(kimchi.total) + "가 나왔습니다.")
+        kimchi.eat()
     elif choice == "피자":
         while b>=1:
             b = b-1
             pizza.sell()
         print("총" + str(pizza.total) + "가 나왔습니다.")
+        pizza.eat()
     else:
         print("메뉴를 다시 선택해주세요")
