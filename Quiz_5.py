d5cbfc4d52b1b21c55db17ebf6af9e72ee0ab191
2018016-Kyungyong-Ryu <144421263+2018016-Kyungyong-Ryu@users.noreply.github.com>
2023-10-13 15:45:08 +0900

    7주차 과제 제출입니다. 붕어빵의 객체는 크림, 팥, 김치, 피자 총 4개이고, input 함수를 사용해 어떤 붕어빵을 총 몇개를 살지를 물어본다음 총 얼마가 나오는지를 출력하는 프로그램을 만들어봤습니다

---
+++ Quiz_5.py
@@ -0,0 +1,49 @@
+class bread:
+    def __init__(self, kind, price):
+        self.kind = kind
+        self.price = price
+        self.total = 0
+
+    def sell(self):
+        print(self.kind + " 붕어빵을 " + str(self.price) + "에 팔았습니다.")
+        self.total = self.total + self.price
+
+    def eat(self):
+        print(self.kind + " 붕어빵 을 먹었습니다.")
+
+# 크림 붕어빵 객체 생성
+cream = bread("슈크림", 2000)
+# 팥 붕어빵 객체 생성
+bean = bread("팥", 1000)
+# 김치 붕어빵 객체 생성
+kimchi = bread("김치", 1500)
+# 피자 붕어빵 객체 생성
+pizza = bread("피자", 1500)
+
+
+
+while True:
+    choice = input("붕어빵을 고르세요 : ")
+    b = int(input("몇개 고르실건가요? : "))
+    if choice == "슈크림":
+        while b>=1:
+            b = b-1
+            cream.sell()
+        print("총 " + str(cream.total) + "이 나왔습니다.")
+    elif choice == "팥":
+        while b>=1:
+            b = b-1
+            bean.sell()
+        print("총 " + str(bean.total) + "가 나왔습니다.")
+    elif choice == "김치":
+        while b>=1:
+            b = b-1
+            kimchi.sell()
+        print("총 " + str(kimchi.total) + "가 나왔습니다.")
+    elif choice == "피자":
+        while b>=1:
+            b = b-1
+            pizza.sell()
+        print("총" + str(pizza.total) + "가 나왔습니다.")
+    else:
+        print("메뉴를 다시 선택해주세요")
